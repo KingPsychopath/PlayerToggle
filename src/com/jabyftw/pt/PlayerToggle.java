997cbff42e21d704547d53438f8949b882c261bc
Jabyftw <rfsantos1996@globo.com>
2014-02-20 06:00:52 -0300

    v0.4 - Always enabled worlds

--- src/com/jabyftw/pt/PlayerToggle.java
+++ src/com/jabyftw/pt/PlayerToggle.java
@@ -1,13 +1,17 @@
 package com.jabyftw.pt;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
@@ -22,22 +26,29 @@ public class PlayerToggle extends JavaPlugin implements Listener {
 
     private ItemStack matE, matD;
     private boolean give;
-    private List<Player> players = new ArrayList();
+    private final List<Player> players = new ArrayList();
+    private final LinkedList<World> alwaysvisible = new LinkedList();
 
     @Override
     public void onEnable() {
+        long start = System.currentTimeMillis();
         FileConfiguration config = getConfig();
         config.addDefault("config.toggleMaterialEnabled", "ink_sack;10;&eHiding players...");
         config.addDefault("config.toggleMaterialDisabled", "ink_sack;8;&4Showing players...");
         config.addDefault("config.giveOnJoin", true);
+        String[] worlds = {"world_nether"};
+        config.addDefault("config.alwaysVisibleWorld", Arrays.asList(worlds));
         config.options().copyDefaults(true);
         saveConfig();
         reloadConfig();
         matE = getItemStack(config.getString("config.toggleMaterialEnabled"));
         matD = getItemStack(config.getString("config.toggleMaterialDisabled"));
         give = config.getBoolean("config.giveOnJoin");
+        for (String s : config.getStringList("config.alwaysVisibleWorld")) {
+            alwaysvisible.add(getServer().getWorld(s));
+        }
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().log(Level.INFO, "Enabled");
+        getLogger().log(Level.INFO, "Enabled in {0}ms", (System.currentTimeMillis() - start));
     }
 
     @Override
@@ -68,24 +79,43 @@ public class PlayerToggle extends JavaPlugin implements Listener {
         }
     }
 
-    private void togglePlayerView(Player p, ItemStack is) {
-        if (!players.contains(p)) {
-            for (Player online : getServer().getOnlinePlayers()) {
-                if (p.canSee(online) && !online.hasPermission("pt.exception")) {
-                    p.hidePlayer(online);
-                }
-            }
-            players.add(p);
-            is.setType(matE.getType());
-            is.setItemMeta(matE.getItemMeta());
-            is.setDurability(matE.getDurability());
-        } else {
+    @EventHandler
+    public void onChangeWorld(PlayerChangedWorldEvent e) {
+        Player p = e.getPlayer();
+        if (alwaysvisible.contains(p.getWorld()) && players.contains(p)) {
             for (Player online : getServer().getOnlinePlayers()) {
                 if (!p.canSee(online)) {
                     p.showPlayer(online);
                 }
             }
             players.remove(p);
+        }
+    }
+
+    private void togglePlayerView(Player p, ItemStack is) {
+        if (!alwaysvisible.contains(p.getWorld())) {
+            if (!players.contains(p)) {
+                for (Player online : getServer().getOnlinePlayers()) {
+                    if (p.canSee(online) && !online.hasPermission("pt.exception")) {
+                        p.hidePlayer(online);
+                    }
+                }
+                players.add(p);
+                is.setType(matE.getType());
+                is.setItemMeta(matE.getItemMeta());
+                is.setDurability(matE.getDurability());
+            } else {
+                for (Player online : getServer().getOnlinePlayers()) {
+                    if (!p.canSee(online)) {
+                        p.showPlayer(online);
+                    }
+                }
+                players.remove(p);
+                is.setType(matD.getType());
+                is.setItemMeta(matD.getItemMeta());
+                is.setDurability(matD.getDurability());
+            }
+        } else {
             is.setType(matD.getType());
             is.setItemMeta(matD.getItemMeta());
             is.setDurability(matD.getDurability());
